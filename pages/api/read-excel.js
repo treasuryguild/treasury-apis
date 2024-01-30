15229416c2618e4d969ee156292767b1f14b3c1d
Andre-Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2024-01-30 09:36:26 +0200

    updated api call

--- pages/api/read-excel.js
+++ pages/api/read-excel.js
@@ -2,34 +2,68 @@
 import ExcelJS from 'exceljs';
 import fetch from 'node-fetch';
 
+async function refreshAccessToken(refreshToken) {
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+
+  const tokenResponse = await fetch('https://oauth2.googleapis.com/token', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: new URLSearchParams({
+      client_id: clientId,
+      client_secret: clientSecret,
+      refresh_token: refreshToken,
+      grant_type: 'refresh_token',
+    }),
+  });
+
+  const tokenData = await tokenResponse.json();
+  return tokenData.access_token;
+}
+
 export default async function handler(req, res) {
   const fileId = process.env.GOOGLE_DOC_ID; 
-  const accessToken = process.env.GOOGLE_ACCESS_TOKEN 
-
-  const url = `https://www.googleapis.com/drive/v3/files/${fileId}?alt=media`;
+  let accessToken = process.env.GOOGLE_ACCESS_TOKEN;
+  const refreshToken = process.env.GOOGLE_REFRESH_TOKEN;
 
   try {
-    const response = await fetch(url, {
-      headers: { Authorization: `Bearer ${accessToken}` },
-    });
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const buffer = await response.buffer();
+    let response = await makeRequest(fileId, accessToken);
 
+    // If token is expired, refresh it
+    if (response.status === 401) { // 401 Unauthorized
+      accessToken = await refreshAccessToken(refreshToken);
+      response = await makeRequest(fileId, accessToken);
+    }
+
+    if (!response.ok) {
+      throw new Error(`Google Drive API Response Error: ${response.status} ${response.statusText}`);
+    }
+
+    const buffer = await response.buffer();
     const workbook = new ExcelJS.Workbook();
     await workbook.xlsx.load(buffer);
 
-    const worksheet = workbook.getWorksheet(1); // Or use the worksheet name
+    const worksheet = workbook.getWorksheet(1);
     let data = [];
-    worksheet.eachRow({ includeEmpty: true }, function (row, rowNumber) {
+    worksheet.eachRow({ includeEmpty: true }, (row, rowNumber) => {
       data.push({ rowNumber, values: row.values });
     });
 
     res.status(200).json({ data });
   } catch (error) {
     console.error('Error:', error);
-    res.status(500).json({ error: 'Failed to read Excel file' });
+    res.status(500).json({
+      error: 'Failed to read Excel file',
+      errorMessage: error.message,
+      stack: error.stack
+    });
   }
   console.log('Done');
+}
+
+async function makeRequest(fileId, accessToken) {
+  const url = `https://www.googleapis.com/drive/v3/files/${fileId}?alt=media`;
+  return fetch(url, {
+    headers: { Authorization: `Bearer ${accessToken}` },
+  });
 }
